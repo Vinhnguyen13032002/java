827091ea32336dd735492afa5a0e13a227155bbd
Vinhnguyen13032002 <97507532+Vinhnguyen13032002@users.noreply.github.com>
2022-02-16 20:15:05 +0700

    baitap

--- baithuchanh1502/src/App.java
+++ baithuchanh1502/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("nhap so nguyen duong");
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int tongcacchuso=0,cacsochuso;
+        cacsochuso=n;
+        while(cacsochuso!=0){
+            tongcacchuso+=cacsochuso%10;
+            cacsochuso=cacsochuso/10;
+        }
+        sc.close();
+        System.out.println("tong so cac chu so nguyen duong la "+ tongcacchuso);
     }
 }
