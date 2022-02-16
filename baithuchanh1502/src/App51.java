84f01f877d798d5526887fe1d47bee9d9e44d969
Vinhnguyen13032002 <97507532+Vinhnguyen13032002@users.noreply.github.com>
2022-02-16 20:18:44 +0700

    baitap

---
+++ baithuchanh1502/src/App51.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App51 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        int n=0;
+        for(n=0;n<5;n++){
+            System.out.println(n);
+        }
+        sc.close();
+    }
+}
